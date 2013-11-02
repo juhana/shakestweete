d6890ed7c517f0bfe5e596d568cc04d7e9f66ae0
Juhana Leinonen <git@nitku.net>
2013-11-03 00:28:07 +0200

    Day 2

--- shakestweete.js
+++ shakestweete.js
@@ -1,6 +1,11 @@
-var fs = require('fs' ),
-    Twit = require('twit' ),
-    twitter;
+var fs = require( 'fs' ),
+    Twit = require( 'twit' ),
+    _ = require( 'underscore' )._;
+
+var twitter,
+    stats = {
+        requests: 0
+    };
 
 var titles = [
     "All's Well That Tweets Well",
@@ -34,6 +39,82 @@ fs.readFile( 'credentials.json', 'utf8', function( error, data ) {
 
     twitter = new Twit( JSON.parse( data ) );
 
-    twitter.get( 'search/tweets', { q: 'alas -filter:links', lang: 'en', count: 2 }, function( error, reply ) {
-    });
+    twitter.get(
+        'search/tweets',
+        { q: 'alas -filter:links', lang: 'en', count: 10 },
+        function( error, reply ) {
+            stats.requests++;
+            if( error ) {
+                throw new Error( error );
+            }
+
+            var tweet;
+
+            // choose the first tweet that fits the bill
+            _.find( reply.statuses, function( data ) {
+                tweet = validateAndClean( data );
+
+                return tweet;
+            });
+
+            if( !tweet ) {
+                console.error( "Suitable seed tweet wasn't found, please try again in a while." );
+                return;
+            }
+
+            console.dir( tweet );
+        }
+    );
 });
+
+
+/**
+ * Check that the tweet is suitable for our purposes and clean it up,
+ * except removing the @s
+ *
+ * @param tweet
+ * @return boolean|object false if unsuitable tweet, a clean tweet object
+ */
+function validateAndClean( tweet ) {
+    // reject users who don't tweet in English
+    if( tweet.user.lang !== 'en' ) {
+        return false;
+    }
+
+    // reject manual retweets
+    if( /\bRT\b/.test( tweet ) ) {
+        return false;
+    }
+
+    // remove trailing hashtags
+    var text = tweet.text.replace( /( #[a-z_0-9]+)+$/i, '' );
+
+    // any hashtags left? Reject if so
+    if( text.indexOf( '#' ) > -1 ) {
+        return false;
+    }
+
+    // Add a comma after the first @
+    text = text.replace( /^(@[a-z_0-9]+) /i, '$1, ' );
+
+    var actor = tweet.user.name;
+
+    // try to clean up the username
+    actor = actor
+        .replace( /_/g, '' )
+        .replace( /\w+/g, function( txt ){
+            return txt.charAt( 0 ).toUpperCase() + txt.substr( 1 ).toLowerCase();
+        });
+
+    // reject if doesn't look like a name
+    if( !/^[a-z\-0-9 .]+$/i.test( actor ) ) {
+        return false;
+    }
+
+    return {
+        text: text,
+        username: tweet.user.screen_name,
+        actor: actor,
+        location: tweet.user.location
+    };
+}
